b39b4478fe8ce368ed8fffb06af9d2ff97fefbe7
Kyle Payawal <payawalkyle@gmail.com>
2025-09-27 01:41:20 +0800

    :sparkles: finished landing

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { defineOptions } from "vue"
+    import Button from "./ui/button/Button.vue";
+
+    defineOptions({
+        name: 'NavBar'
+    });
+</script>
+
+<template>
+    <nav class="bg-none border-gray-200 dark:bg-gray-900 absolute top-0 left-0 w-full z-10">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <span class="self-center text-3xl font-bold whitespace-nowrap dark:text-white">SplitSmart</span>
+        </a>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button type="button" class="text-black bg-none border border-black hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center shadow-md">Sign In</button>
+            <Button type="button" class="text-white border border-blue-700 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mx-2">Get started</Button>
+        </div>
+        </div>
+    </nav>
+</template>
---
+++ frontend/src/pages/Landing.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineOptions } from "vue"
+import NavBar from "../components/NavBar.vue";
+
+defineOptions({
+    name: 'Landing'
+});
+
+</script>
+
+<template>
+  <NavBar/>
+  <main class="w-full h-full">
+    <div class="flex items-center justify-start p-30 landing-background absolute inset-0 w-screen h-screen">
+      <div class="flex flex-col items-start z-10 max-w-4xl w-3/4 pr-8">
+        <div>
+          <h1 class="text-xl md:text-8xl sm:text-2xl leading-tight font-bold text-left text-[#474747] mb-8">Worry Less About the Bill, Enjoy More of the Moment</h1>
+        </div>
+        <div>
+          <button class="px-30 py-4 bg-white text-gray-700 rounded-full shadow-md hover:shadow-lg transition-shadow text-2xl hover:bg-blue-500 hover:text-white">Get Started</button>
+        </div>
+      </div>
+      <div class="absolute right-8 bottom-0 w-3/5 flex justify-end">
+        <img src="/landing.png" alt="Coins and receipt" class="h-auto max-h-full object-contain"/>
+      </div>
+    </div>
+  </main>
+</template>
